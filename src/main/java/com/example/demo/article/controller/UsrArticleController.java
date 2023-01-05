469551198f297b14af76b3494c234feb371aa918
SungHoon77 <ssfu777@gmail.com>
2023-01-05 23:45:41 +0900

    for문을 이용한 getArticles

---
+++ src/main/java/com/example/demo/article/controller/UsrArticleController.java
@@ -0,0 +1,53 @@
+package com.example.demo.article.controller;
+
+import com.example.demo.article.vo.Article;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+
+public class UsrArticleController {
+  int articleLastId;
+  private List<Article> articles;
+
+  public UsrArticleController() {
+    articles = new ArrayList<>();
+    articleLastId = 0;
+    makeTestData();
+  }
+
+  private void makeTestData() {
+    for(int i=1; i<=10; i++){
+      int id = articleLastId +1;
+      String title = "제목" + i;
+      String body = "내용" + i;
+      Article article = new Article(id,title,body);
+      articles.add(article);
+      articleLastId = id;
+
+    }
+  }
+
+  @RequestMapping("/usr/article/doAdd")
+  @ResponseBody
+  public Article doAdd(String title, String body) {
+    int id = articleLastId + 1;
+    Article article = new Article(id, title, body);
+
+    articles.add(article);
+    articleLastId = id;
+
+    return article;
+  }
+
+  @RequestMapping("/usr/article/getarticles")
+  @ResponseBody
+  public List<Article> getarticles() {
+    return articles;
+  }
+}
--- src/main/java/com/example/demo/home/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/home/controller/UsrHomeController.java
@@ -1,20 +1,36 @@
-package com.example.demo.artiicle.controller;
+package com.example.demo.home.controller;
+
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 
 public class UsrHomeController {
-  private int count = 0;
-  public UsrHomeController(){
-    count = 0;
-  }
-  @RequestMapping("/usr/home/main")
-  @ResponseBody
-  public int showMain(){
 
-    return ++count;
+
+  @RequestMapping("/usr/home/getMap")
+  @ResponseBody
+  public Map<String, Object> getMap(){
+    Map<String, Object> map = new HashMap<>();
+    map.put("철수나이", 22);
+    map.put("영희나이",21);
+    return map;
   }
+  @RequestMapping("/usr/home/getList")
+  @ResponseBody
+  public List<String> getList(){
+    List<String> list = new ArrayList<>();
+    list.add("철수");
+    list.add("영희");
+    return list;
+  }
+
+
 }
