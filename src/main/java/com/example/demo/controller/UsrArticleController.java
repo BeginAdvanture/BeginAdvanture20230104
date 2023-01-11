cb5d8a936a37b21d3f2ffdd02d14d0fc5be617fa
SungHoon77 <ssfu777@gmail.com>
2023-01-11 14:51:16 +0900

    C:/Program Files/Git/usr/article/getArticle에 ResultData 적용

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controller;
 
 import com.example.demo.service.ArticleService;
+import com.example.demo.utill.Ut;
 import com.example.demo.vo.Article;
+import com.example.demo.vo.ResultData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,12 +30,12 @@ public class UsrArticleController {
 
   @RequestMapping("/usr/article/getArticle")
   @ResponseBody
-  public Object getArticle(int id) {
+  public ResultData getArticle(int id) {
     Article article = articleService.getArticle(id);
     if( article == null){
-      return  id + "게시물이 존재하지 않습니다.";
+      return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.",id));
     }
-    return article;
+    return ResultData.from("S-1", Ut.f("%d번 게시물입니다.",id));
   }
 
 
