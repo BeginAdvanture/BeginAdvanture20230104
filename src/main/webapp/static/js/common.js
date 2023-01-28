900a2b8059f8b7c98ad0f006ddd20345da819092
SungHoon77 <ssfu777@gmail.com>
2023-01-28 18:14:44 +0900

    검색박스 구현

---
+++ src/main/webapp/static/js/common.js
@@ -0,0 +1,11 @@
+$('select[data-value]').each(function(index, el){
+    const = $el= $(el);
+
+
+
+    const defaultValue = $el.attr('data-value');
+    if(defaultValue.length >0){
+      $el.val(defaultValue);
+    }
+
+});
